ee4a5abc06db7d1fc9ea21dfc4ee4b7e7be5e91b
psittacine <psittacine714@gmail.com>
2020-12-06 03:07:58 -0600

    Initialize Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-// import
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from './actions/shared'
 
-function App() {
-  return (
-    <div>
-      Placeholder
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>
+        Placeholder
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect()(App)
