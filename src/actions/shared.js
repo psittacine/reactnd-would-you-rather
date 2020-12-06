5e0beb2cc14edab1c5b3acb6a167a042768304a1
psittacine <psittacine714@gmail.com>
2020-12-06 05:27:07 -0600

    Add LoadingBar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { getUsers } from '../actions/users'
 import { getQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
             .then(({ users, questions }) => {
                 dispatch(getUsers(users))
                 dispatch(getQuestions(questions))
                 dispatch(setAuthedUser(AUTHED_ID))
+                dispatch(hideLoading())
             })
     }
 }
